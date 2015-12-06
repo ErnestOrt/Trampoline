379ed94c2a70245c5df26633721f0bba0fca5551
Ernest Ortuño <ernest.ort.tor@gmail.com>
2015-12-06 18:51:10 +0100

    * Adding spring boot neture to the project * Serving Web Content with Spring MVC

---
+++ trampoline/src/main/java/org/ernest/applications/trampoline/controller/ViewsController.java
@@ -0,0 +1,16 @@
+package org.ernest.applications.trampoline.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ViewsController {
+
+	@RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
