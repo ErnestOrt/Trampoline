351e83c9ad177d9dc05eb905f9125d12cd5f484e
Ernest Ortuño <ernest.ort.tor@gmail.com>
2015-12-06 20:27:41 +0100

    adding static home page

--- trampoline/src/main/java/org/ernest/applications/trampoline/controller/ViewsController.java
+++ trampoline/src/main/java/org/ernest/applications/trampoline/controller/ViewsController.java
@@ -3,14 +3,12 @@ package org.ernest.applications.trampoline.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ViewsController {
 
-	@RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+	@RequestMapping("/trampoline")
+    public String greeting(Model model) {
+        return "index";
     }
 }
