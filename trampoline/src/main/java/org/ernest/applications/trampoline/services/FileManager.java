8ad44d378a3f83c4463c523f671000d2ca0a3878
Ernest Ortuño <ernest.ort.tor@gmail.com>
2015-12-09 21:36:08 +0100

    adding requirments to being able to run it on Linux & Windows

--- trampoline/src/main/java/org/ernest/applications/trampoline/services/FileManager.java
+++ trampoline/src/main/java/org/ernest/applications/trampoline/services/FileManager.java
@@ -10,6 +10,7 @@ import org.ernest.applications.trampoline.exceptions.CreatingSettingsFolderExcep
 import org.ernest.applications.trampoline.exceptions.ReadingEcosystemException;
 import org.ernest.applications.trampoline.exceptions.RunningMicroserviceScriptException;
 import org.ernest.applications.trampoline.exceptions.SavingEcosystemException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.google.gson.Gson;
@@ -18,48 +19,48 @@ import com.google.gson.JsonSyntaxException;
 @Component
 public class FileManager {
 
-	private static final String TRAMPOLINE_FOLDER = "/Users/"+System.getProperties().getProperty("user.name")+"/Documents/trampoline";
-	private static final String TRAMPOLINE_SETTINGS_PATH = TRAMPOLINE_FOLDER + "/settings.txt";
+	@Value("${settings.folder.path.mac}")
+	private String settingsFolderPathMac;
 	
+	@Value("${settings.folder.path.linux}")
+	private String settingsFolderPathLinux;
+	
+	@Value("${settings.folder.path.linux}")
+	private String settingsFolderPathWindows;
+	
+	@Value("${settings.file.name}")
+	private String settingsFileName;
+
 	public Ecosystem getEcosystem() throws CreatingSettingsFolderException, ReadingEcosystemException {
 		checkIfFileExistsAndCreatedIfNeeded();
 		Ecosystem ecosystem = null;
 		
 		try {
-			ecosystem = new Gson().fromJson(FileUtils.readFileToString(new File(TRAMPOLINE_SETTINGS_PATH)), Ecosystem.class);
+			ecosystem = new Gson().fromJson(FileUtils.readFileToString(new File(getSettingsFolder() + "/" + settingsFileName)), Ecosystem.class);
 		} catch (JsonSyntaxException | IOException e) {
 			e.printStackTrace();
 			throw new ReadingEcosystemException();
 		}
-		
 		return ecosystem;
 	}
 	
 	public void saveEcosystem(Ecosystem ecosystem) throws SavingEcosystemException {
 		try {
-			FileUtils.writeStringToFile(new File(TRAMPOLINE_SETTINGS_PATH), new Gson().toJson(ecosystem));
+			FileUtils.writeStringToFile(new File(getSettingsFolder() + "/" + settingsFileName), new Gson().toJson(ecosystem));
 		} catch (IOException e) {
 			e.printStackTrace();
 			throw new SavingEcosystemException();
 		}
 	}
-	
-	private void checkIfFileExistsAndCreatedIfNeeded() throws CreatingSettingsFolderException {
-		try{
-			File file = new File(TRAMPOLINE_FOLDER);
-			if(!file.exists()){			
-				file.mkdirs();
-				FileUtils.writeStringToFile(new File(TRAMPOLINE_SETTINGS_PATH), new Gson().toJson(new Ecosystem()));
-			}
-		}catch(Exception e){
-			e.printStackTrace();
-			throw new CreatingSettingsFolderException();
-		}
-	}
 
 	public void runScript(String id, String mavenLocation, String port) throws RunningMicroserviceScriptException {
 		try {
-			new ProcessBuilder("sh", TRAMPOLINE_FOLDER + "/" + id + ".sh", mavenLocation, port).start();
+			if(System.getProperties().getProperty("os.name").contains("Windows")){
+				new ProcessBuilder("cmd", "/c", "start", getSettingsFolder() + "/" + id + ".bat", mavenLocation, port).start();
+			}else{
+				new ProcessBuilder("sh", getSettingsFolder() + "/" + id + ".sh", mavenLocation, port).start();
+			}
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 			throw new RunningMicroserviceScriptException();
@@ -68,10 +69,38 @@ public class FileManager {
 
 	public void createScript(String id, String pomLocation) throws CreatingMicroserviceScriptException {
 		try {
-			FileUtils.writeStringToFile(new File(TRAMPOLINE_FOLDER +"/"+ id +".sh"), "export M2_HOME=$1; export PATH=$PATH:$M2_HOME/bin; cd " + pomLocation + "; mvn spring-boot:run -Dserver.port=$2;");
+			if(System.getProperties().getProperty("os.name").contains("Windows")){
+				FileUtils.writeStringToFile(new File(getSettingsFolder() +"/"+ id +".bat"), "set M2_HOME=$1; set PATH=$PATH:$M2_HOME/bin; cd " + pomLocation + "; mvn spring-boot:run -Dserver.port=$2;");
+			}else{
+				FileUtils.writeStringToFile(new File(getSettingsFolder() +"/"+ id +".sh"), "export M2_HOME=$1; export PATH=$PATH:$M2_HOME/bin; cd " + pomLocation + "; mvn spring-boot:run -Dserver.port=$2;");
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 			throw new CreatingMicroserviceScriptException();
+		}
+	}
+	
+	private String getSettingsFolder() {
+		
+		if(System.getProperties().getProperty("os.name").contains("Mac")){
+			return settingsFolderPathMac.replaceAll("#userName", System.getProperties().getProperty("user.name"));
+		}else if(System.getProperties().getProperty("os.name").contains("Windows")){
+			return settingsFolderPathWindows;
+		}else{
+			return settingsFolderPathLinux.replaceAll("#userName", System.getProperties().getProperty("user.name"));
+		}
+	}
+
+	private void checkIfFileExistsAndCreatedIfNeeded() throws CreatingSettingsFolderException {
+		try{
+			File file = new File(getSettingsFolder());
+			if(!file.exists()){			
+				file.mkdirs();
+				FileUtils.writeStringToFile(new File(getSettingsFolder() + "/" + settingsFileName), new Gson().toJson(new Ecosystem()));
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+			throw new CreatingSettingsFolderException();
 		}
 	}
 }
