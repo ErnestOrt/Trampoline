a3868789b80d91ba3e6806cf8e321267e5eed383
Ernest Ortuño <ernest.ort.tor@gmail.com>
2017-08-27 13:55:05 +0200

    Adding gradle microservice exampel

---
+++ microservice-example-gradle/src/test/java/org/ernest/applications/microserviceexamplegradle/MicroserviceExampleGradleApplicationTests.java
@@ -0,0 +1,16 @@
+package org.ernest.applications.microserviceexamplegradle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceExampleGradleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
