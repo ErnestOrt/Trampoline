527f45827e85c07c07165d92e62146267eab9945
Ernest Ortuño <ernest.ort.tor@gmail.com>
2018-03-03 10:08:42 +0100

    code review

--- trampoline/src/main/java/org/ernest/applications/trampoline/controller/TrampolineManagerController.java
+++ trampoline/src/main/java/org/ernest/applications/trampoline/controller/TrampolineManagerController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Component()
+@Component
 @RequestMapping("/trampoline")
 public class TrampolineManagerController {
 
