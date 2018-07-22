b2e0286f794891117f9b831a6a8a2c179397828f
DRAGONBORN\Tyler <tylerbthrailkill@gmail.com>
2018-07-21 19:28:05 -0600

    GitCredentials now contain HTTPS and SSH settings
    Initialize GitCredentials to an empty object so no need for null checks

--- trampoline/src/main/java/org/ernest/applications/trampoline/entities/GitCredentials.java
+++ trampoline/src/main/java/org/ernest/applications/trampoline/entities/GitCredentials.java
@@ -2,30 +2,82 @@ package org.ernest.applications.trampoline.entities;
 
 public class GitCredentials {
 
-    private String username;
-    private String pass;
+    private HttpsSettings httpsSettings;
+    private SshSettings sshSettings;
 
     public GitCredentials() {
     }
 
+    public GitCredentials(String username, String pass, String sshKeyLocation) {
+        httpsSettings = new HttpsSettings(username, pass);
+        sshSettings = new SshSettings(sshKeyLocation);
+    }
+
     public GitCredentials(String username, String pass) {
-        this.username = username;
-        this.pass = pass;
+        httpsSettings = new HttpsSettings(username, pass);
     }
 
-    public String getUsername() {
-        return username;
+    public GitCredentials(String sshKeyLocation) {
+        sshSettings = new SshSettings(sshKeyLocation);
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public HttpsSettings getHttpsSettings() {
+        return httpsSettings;
     }
 
-    public String getPass() {
-        return pass;
+    public void setHttpsSettings(HttpsSettings httpsSettings) {
+        this.httpsSettings = httpsSettings;
     }
 
-    public void setPass(String pass) {
-        this.pass = pass;
+    public SshSettings getSshSettings() {
+        return sshSettings;
+    }
+
+    public void setSshSettings(SshSettings sshSettings) {
+        this.sshSettings = sshSettings;
+    }
+
+    public class HttpsSettings {
+        private String username;
+        private String pass;
+
+        public HttpsSettings(String username, String pass) {
+            this.username = username;
+            this.pass = pass;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPass() {
+            return pass;
+        }
+
+        public void setPass(String pass) {
+            this.pass = pass;
+        }
+
+    }
+
+    public class SshSettings {
+        private String sshKeyLocation;
+
+        public SshSettings(String sshKeyLocation) {
+            this.sshKeyLocation = sshKeyLocation;
+        }
+
+        public String getSshKeyLocation() {
+            return sshKeyLocation;
+        }
+
+        public void setSshKeyLocation(String sshKeyLocation) {
+            this.sshKeyLocation = sshKeyLocation;
+        }
+
     }
 }
